fadaf240835f34830041a1cfbf625d4f8617ef8a
Louis Blanc <142250550+YenchiSomnambule@users.noreply.github.com>
2026-05-18 20:00:27 -0500

    Add base path configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
+    base: process.env.VITE_BASE_PATH || '/',
+    plugins: [react()],
+    server: {
+          host: true,
+          port: 5173,
+    },
 });
